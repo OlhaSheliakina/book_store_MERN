35fc419ca76eaad7d612f50970b9b6ad57008815
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-10-23 00:16:54 +0300

    adding post method for adding new book

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,51 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { PORT, MONGO_DB_URL } from './config.js';
+import { Book } from './models/bookModel.js';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
   console.log(request);
   return response.status(234).send('Welcome to the Book Store');
 });
 
-  mongoose
-    .connect(MONGO_DB_URL)
-    .then(() => {
-      console.log('App connected to database');
-      app.listen(PORT, () => {
-        console.log(`App is listening to port: ${PORT}`);
+app.post('/books', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
       });
-    })
-    .catch((error) => {
-console.log(error)
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(MONGO_DB_URL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });

ab072d7fb64a767d4e3ee79b8a53d6b5a5aa116d
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-10-23 22:51:40 +0300

    adding routes for getting book by id and for updating a book

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,51 @@ app.get('/books', async (request, response) => {
   }
 });
 
+app.get('/books/:id', async (request, response) => {
+  try {
+
+    const { id } = request.params;
+
+    const books = await Book.findById(id);
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+app.put('/books/:id', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: 'Book was not found' });
+    }
+
+    return response.status(200).send({ message: 'Book was updated sucessfully' }); 
+
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(MONGO_DB_URL)
   .then(() => {

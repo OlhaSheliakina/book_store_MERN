701a76c71d0698b2b42b7452c92e4cbcc9742acd
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-10-30 16:44:22 +0200

    adding Book type, creating Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import Spinner from '../components/Spinner';
+import { Book } from '../types/bookType';
 
 const Home = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get('http://localhost:5555/books')
+      .then((response) => {
+        setBooks(response.data.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className='p-4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>Books List</h1>
+        <Link to='/books/create'>
+          <MdOutlineAddBox className='text-sky-800 text-4xl' />
+        </Link>
+      </div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <table className='w-full border-separate border-spacing-2'>
+          <thead>
+            <tr>
+              <th className='border border-slate-600 rounded-md'>No</th>
+              <th className='border border-slate-600 rounded-md'>Title</th>
+              <th className='border border-slate-600 rounded-md max-md:hidden'>
+                Author
+              </th>
+              <th className='border border-slate-600 rounded-md max-md:hidden'>
+                Publish Year
+              </th>
+              <th className='border border-slate-600 rounded-md'>Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => (
+              <tr key={book.id} className='h-8'>
+                <td className='border border-slate-700 rounded-md text-center'>
+                  {index + 1}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center'>
+                  {book.title}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                  {book.author}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                  {book.publishYear}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center'>
+                  <div className='flex justify-center gap-x-4'>
+                    <Link to={`/books/details/${book.id}`}>
+                      <BsInfoCircle className='text-2xl text-green-800' />
+                    </Link>
+                    <Link to={`/book/edit/${book.id}`}>
+                      <AiOutlineEdit className='text-2xl text-yellow-600' />
+                    </Link>
+                    <Link to={`/books/delete/${book.id}`}>
+                      <MdOutlineAddBox className='text-2xl text-red-600' />
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
 
 export default Home;

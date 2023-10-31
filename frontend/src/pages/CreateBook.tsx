1449d4badcdb3b10c1b07c037bdb970e6ca7ecc1
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-10-31 20:33:46 +0200

    adding create book form and functionality

--- frontend/src/pages/CreateBook.tsx
+++ frontend/src/pages/CreateBook.tsx
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
 
 const CreateBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setIsLoading(true);
+    axios
+      .post('http://localhost:5555/books', data)
+      .then(() => {
+        setIsLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        alert('An error has occured. Please check the console');
+        console.log(error);
+      });
+  };
+
   return (
-    <div>CreateBook</div>
-  )
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Created Book</h1>
+      {isLoading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4'>Title</label>
+          <input
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-3 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4'>Author</label>
+          <input
+            type='text'
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-3 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4'>Publish Year</label>
+          <input
+            type='text'
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-3 w-full'
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CreateBook;
